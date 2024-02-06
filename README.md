a1d4ac9024cfc9c030fb83e510821728c68d01b9
mirdukkkkk <karmulav@gmail.com>
2024-02-07 05:07:57 +0700

    fix & add random color

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+# Site with my links & contacts
+You can see an already live version at https://mirdukkkkkkkk.space
+
+## How to run?
+Just make a local server on `./docs/index.html` (or open index.html in your browser)
+
+---
+
+## It works just as well without JavaScript
+You can disable JavaScript on the site, it will work the same way, except with a random highlight color and no starry background
--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,25 @@
-const welcome = [
-    "hello",
-    "hello there",
-    "hi",
-    "hey",
-    "g'day",
-    "howdy",
-    "peek-a-boo",
-    "hey there",
-    "yo"
-];
+document.addEventListener('DOMContentLoaded', function() {
+    const greetings = [
+        "hello",
+        "hello there",
+        "hi",
+        "hey",
+        "g'day",
+        "howdy",
+        "peek-a-boo",
+        "hey there",
+        "yo"
+    ];
 
-document.getElementById('greeting').innerText = welcome[Math.floor(Math.random() * welcome.length)];
+    const colors = [
+        "#1755ff",
+        "#50c257",
+        "#9f37c2",
+        "#f1d04f",
+        "#ff3737",
+        "#7fffec"
+    ];
+
+    document.documentElement.style.setProperty('--text-highlight-color', colors[Math.floor(Math.random() * colors.length)])
+    document.getElementById('greeting').innerText = greetings[Math.floor(Math.random() * greetings.length)];
+})
