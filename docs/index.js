fe572d259b92083df202bf9f4c51b40191a1b132
mirdukkkkk <contact@mirdukkkkk.space>
2024-04-28 02:52:41 +0700

    some animations and decorations :cherry_blossom:

--- docs/index.js
+++ docs/index.js
@@ -1,25 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const greetings = [
-        "hello",
-        "hello there",
-        "hi",
-        "hey",
-        "g'day",
-        "howdy",
-        "peek-a-boo",
-        "hey there",
-        "yo"
-    ];
+const greetings = [
+    "hello",
+    "hello there",
+    "hi",
+    "hey",
+    "g'day",
+    "howdy",
+    "peek-a-boo",
+    "hey there",
+    "yo"
+];
+const colors = [
+    "#1755ff",
+    "#50c257",
+    "#9f37c2",
+    "#f1d04f",
+    "#ff3737",
+    "#7fffec"
+];
 
-    const colors = [
-        "#1755ff",
-        "#50c257",
-        "#9f37c2",
-        "#f1d04f",
-        "#ff3737",
-        "#7fffec"
-    ];
+function switchColor(random = false) {
+    document.documentElement.style.setProperty(
+        '--text-highlight-color',
+        random
+            ? colors[Math.floor(Math.random() * colors.length)]
+            : colors[(colors.indexOf(document.documentElement.style.getPropertyValue('--text-highlight-color')) + 1) % colors.length]
+    );
+}
 
-    document.documentElement.style.setProperty('--text-highlight-color', colors[Math.floor(Math.random() * colors.length)])
-    document.getElementById('greeting').innerText = greetings[Math.floor(Math.random() * greetings.length)];
-})
+document.getElementById('greeting').innerText = greetings[Math.floor(Math.random() * greetings.length)];
+switchColor(true);
